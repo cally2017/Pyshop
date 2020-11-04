fd0520d1cd694d26412ef45d53429be5bf979b43
Binh <bttbinh@gmail.com>
2020-11-03 21:27:29 -0500

    finish

---
+++ products/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.CharField(max_length=50)
+    stock = models.IntegerField()
+    image_url = models.CharField(max_length=2083)
+
+
+class Offer(models.Model):
+    code = models.CharField(max_length=10)
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+
+
+
